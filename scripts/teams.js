7dbe5244e290cacbfdca568e51a3f1830515607f
Ferraro <27ferrarod@mydacc.org>
2026-05-13 08:40:30 -0400

    Dom added reviews page, changed header navigation, and refined teams page.

--- scripts/teams.js
+++ scripts/teams.js
@@ -3,19 +3,19 @@ const teamData = {
   "Dominic Johnson": {
     role: "H.R. Management",
     desc: "Dominic handles employee relations, hiring, onboarding, and workplace policies. He enjoys hiking and coffee tasting on weekends.",
-    image: "img/icon.jpg",
+    image: "img/Dominic_J.jpg",
   },
 
   "Jenny Tarmack": {
     role: "C.E.O.",
     desc: "Jenny oversees company operations and long-term strategy. She founded GreenScape Solutions and is passionate about sustainability.",
-    image: "img/icon.jpg",
+    image: "img/Jenny_T.jpg",
   },
 
   "Sarah Williams": {
     role: "Marketing Director",
     desc: "Sarah manages advertising campaigns, social media, and branding efforts. She loves photography and graphic design.",
-    image: "img/icon.jpg",
+    image: "img/Sarah_W.jpg",
   },
 };
 
